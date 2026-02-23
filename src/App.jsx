1a3540dec058cdc52d13310a18e623c382a27e7c
Mateo Julio Gomero Rios <mateogomerorios@gmail.com>
2026-02-22 19:08:23 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
-import Header from './components/Header' // 👈 IMPORTAR
 
 export default function App() {
   return (
-    <div>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
   )
 }
